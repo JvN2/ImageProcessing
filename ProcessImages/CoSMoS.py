8e4c1ec205e1ba5aee45c389e0050508812d37f3
noort <noort@physics.leidenuniv.nl>
2022-04-08 18:39:25 +0200

    Read in channels

--- ProcessImages/CoSMoS.py
+++ ProcessImages/CoSMoS.py
@@ -5,6 +5,7 @@ from PIL import ImageDraw
 from imaris_ims_file_reader.ims import ims
 from scipy import fftpack
 from tqdm import tqdm
+import h5py
 
 from ProcessImages.ImageIO import find_peaks, create_circular_mask
 from ProcessImages.ImageIO2 import merge_rgb_images, scale_u8
@@ -78,43 +79,61 @@ def filter(image_stack, highpass=5, low_pas=None):
     return image_stack
 
 
-def save_rgb(filename, image_stack, t=0, peaks=None, radius=10):
-    im_rgb = []
-    for color in range(3):
+def save_rgb(filename, image_stack, channels, t=0, peaks=None, radius=10, numbers = False):
+    im_rgb = np.zeros([3, np.shape(image_stack)[-2], np.shape(image_stack)[-1]])
+    image_colors = {'637': 0, '561': 1, '488': 2}
+    for color, channel in enumerate(channels):
         image = image_stack[t, color, 0, :, :]
         std = get_background(image)
-        im_rgb.append(scale_u8(image, [-std, 20 * std]))
+        im_rgb[image_colors[channel]] = scale_u8(image, [-std, 20 * std])
     im = merge_rgb_images(im_rgb[0] * 0, im_rgb[0], im_rgb[1], im_rgb[2])
 
-    for i, c in enumerate(peaks):
-        bbox = (c[0] - radius, c[1] - radius, c[0] + radius, c[1] + radius)
+    for i, coord in enumerate(peaks):
+        bbox = (coord[0] - radius, coord[1] - radius, coord[0] + radius, coord[1] + radius)
         draw = ImageDraw.Draw(im)
         draw.ellipse(bbox, fill=None, outline='white')
-        bbox = list(np.clip(c+radius/np.sqrt(2), 0, np.shape(image_stack)[-1]))
-        draw.text(bbox, str(i), color='white')
+        bbox = list(np.clip(coord+radius/np.sqrt(2), 0, np.shape(image_stack)[-1]))
+        if numbers:
+            draw.text(bbox, str(i), color='white')
     del draw
     im.save(filename.replace('.ims', f'_{t}.jpg'))
 
+def read_channels(filename):
+    channels = []
+    with h5py.File(filename, 'r') as hdf:
+        for channel in range(6):
+            try:
+                channels.append(hdf[rf'DataSetInfo/Channel {channel}'].attrs['LSMExcitationWavelength'][:3].tobytes().decode())
+            except KeyError:
+                return channels
+
+
 
 filename = r'C:\Users\noort\Downloads\Plexi_Channel2_FOV4_dsDNAPol1_2022-03-21_Protocol 4_18.56.31.ims'
+filename = r'C:\Users\noort\Downloads\Slide2_Channel1_FOV2_512_Int100_Exp50_Rep05_Pol1_2022-04-06_Protocol 2_16.32.11.ims'
 print(f'Opening file: {filename}')
+
 image_stack = ims(filename)
-image_stack = cut_roi(image_stack, roi_width=1000)
+channels = read_channels(filename)
+print(channels)
+
+image_stack = cut_roi(image_stack, roi_width=150)
 print('Filtering ....')
 image_stack = filter(image_stack, highpass=5)
 
-radius = 6
-color = 1
+
+radius = 5
 print('Finding peaks...')
-peaks = find_peaks(image_stack[0, color, 0, :, :], radius, n_traces=10000, treshold_sd=2)
+peaks = find_peaks(image_stack[0, channels.index('561'), 0, :, :], radius, n_traces=10000, treshold_sd=2)
 
 for t in range(image_stack.shape[0]):
-    save_rgb(filename, image_stack, t, peaks, radius)
+    save_rgb(filename, image_stack, channels, t, peaks, radius)
 
 traces = get_traces(image_stack, peaks, radius)
+plot_colors = {'637': 'r', '561': 'g', '488':'b'}
 for trace_nr, trace in enumerate(traces):
-    for i, c in enumerate(['r', 'g', 'b']):
-        plt.plot(trace[i], color=c)
+    for i, channel in enumerate(channels):
+        plt.plot(trace[i], color=plot_colors[channel])
     plt.title(f'peak {trace_nr}')
     plt.ylim(-200, 2500)
     plt.show()
@@ -122,7 +141,7 @@ for trace_nr, trace in enumerate(traces):
 
 column_names = []
 for trace_nr, trace in enumerate(traces):
-    for i, c in enumerate(['r', 'g', 'b']):
-       column_names.append(f'{trace_nr}: {c}')
+    for i, channel in enumerate(channels):
+       column_names.append(f'{trace_nr}: {channel}nm')
 df = pd.DataFrame(np.reshape(traces, (-1, np.shape(traces)[-1])).T, columns=column_names)
 df.to_csv(filename.replace('ims', 'csv'))
